61ae0cd3f7c3fd93e52088adf69e0c6f03028910
Arthur Picht <picht@arthurpicht.de>
2025-03-05 15:39:20 +0100

    adjustment due to cli refactoring: backup

--- src/main/java/org/mentalizr/infra/taskAgent/BackupTaskAgent.java
+++ src/main/java/org/mentalizr/infra/taskAgent/BackupTaskAgent.java
@@ -1,8 +1,9 @@
 package org.mentalizr.infra.taskAgent;
 
-import org.mentalizr.cli.adminApi.AdminApiException;
-import org.mentalizr.cli.adminApi.CliExternalApi;
-import org.mentalizr.cli.adminApi.Session;
+import org.mentalizr.clientSdk.ClientSdkException;
+import org.mentalizr.clientSdk.SessionAgent;
+import org.mentalizr.clientSdk.backup.Backup;
+import org.mentalizr.clientSdk.backup.BackupRequest;
 import org.mentalizr.commons.paths.host.hostDir.BackupDestinationDir;
 import org.mentalizr.infra.InfraRuntimeException;
 
@@ -11,10 +12,13 @@ public class BackupTaskAgent {
     public static void backup() {
         BackupDestinationDir backupDestinationDir = new BackupDestinationDir();
         try {
-            Session.loginWithLocalConfiguration();
-            CliExternalApi.backup(backupDestinationDir.asPath());
-            Session.logout();
-        } catch (AdminApiException e) {
+            BackupRequest backupRequest = new BackupRequest.Builder()
+                    .withDestinationDir(backupDestinationDir.asPath())
+                    .build();
+            SessionAgent sessionAgent = SessionAgent.createFromLocalConfigWithTransientCookieStorage();
+            Backup.exec(sessionAgent.getRESTCallContext(), backupRequest);
+            sessionAgent.logout();
+        } catch (ClientSdkException e) {
             throw new InfraRuntimeException("Backup failed. " + e.getMessage(), e);
         }
     }
